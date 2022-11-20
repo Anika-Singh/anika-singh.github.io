42042ce7d062cee432cd258f16289d1dcaede71c
Anika-Singh <anikasingh2001@gmail.com>
2022-11-20 10:26:44 -0600

    Updates

---
+++ precache-manifest.0a9ae1f4a33024129d7e8f1f1fb2ea12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c18527034060417276b13433b35bf7",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "6007d24376d47d2be060",
+    "url": "/home/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2cbb6ddbc98e8f24cf7e",
+    "url": "/home/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "6007d24376d47d2be060",
+    "url": "/home/static/js/2.6a586009.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/home/static/js/2.6a586009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbb6ddbc98e8f24cf7e",
+    "url": "/home/static/js/main.ac1b14f9.chunk.js"
+  },
+  {
+    "revision": "bac681e6707b590225d4",
+    "url": "/home/static/js/runtime-main.caf7a1cd.js"
+  }
+]);
